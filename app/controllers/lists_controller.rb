277930f3a8379ff948869ebb23bae0ce4109d478
Edward Toonela <edward.toonela@icloud.com>
2022-02-24 15:58:41 -0300

    completed list controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,32 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :bookmark, :movies)
+  end
+
 end
