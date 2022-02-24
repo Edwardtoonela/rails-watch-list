7cfcb0b07d04d7902f73b4aaf8a90d4caba41fe6
Edward Toonela <edward.toonela@icloud.com>
2022-02-24 14:06:41 -0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+
+  resources :lists do
+    collection do
+      get :top
+    end
+  end
+
+  resources :lists do
+    member do
+      get :bookmarks
+      get :movies
+    end
+  end
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
