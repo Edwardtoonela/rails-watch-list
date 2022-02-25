4f616652f47fd2d1839390b2e8e48749b2c56c06
Edward Toonela <edward.toonela@icloud.com>
2022-02-24 22:31:18 -0300

    added bookmarked movie card, add movie btn, remove btn on user list viewer page

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,45 @@
 class BookmarksController < ApplicationController
 
   def new
-    @bookmark = Bookmark.new(bookmark.params)
-    @list = List.create(bookmark: @bookmark)
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+      # we need `restaurant_id` to associate review with corresponding restaurant
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+    if @bookmark.update(bookmark_params) #this will already save your nested project along with the bookmark
+      redirect_to list_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
   end
 end
